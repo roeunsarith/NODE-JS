1a831e27cd9c5e5b16987356c14f5757f4b9f3db
roeun sarith <roeunsarith168@gmail.com>
2022-12-05 09:05:12 +0700

    test

--- src/controller/test.controller.js
+++ src/controller/test.controller.js
@@ -2,7 +2,7 @@ const getlist = (req,res) =>{
      res.send(">>>get list");
 }
 const addlist = (req,res) =>{
-    res.send(">>>add list");
+    res.send(">>>add list Hello");
 }
 module.exports ={
     getlist,addlist
